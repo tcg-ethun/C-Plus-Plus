7aab0579c6e2125a9bbed2d83407698e63d4fd5d
tcg-ethun <ethuntcg@gmail.com>
2025-07-02 08:39:01 +0600

    Updated File

---
+++ 2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a ,b ,sum;
+    cout << "Enter the first number : ";
+  cin >> a;
+  cout << "Enter the second number : ";
+  cin >> b;
+  sum = a+b;
+  cout << "The sum is : " << sum;
+return 0;
+}
---
+++ 3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int a,b,subs;
+  cout << "Enter num1 :";
+  cin >> a;
+  cout << "Enter num2 :";
+  cin >> b;
+  subs = a-b;
+  cout << "Your ans is : " << subs;
+return 0;
+}
