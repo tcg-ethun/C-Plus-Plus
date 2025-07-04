4556e315210c20987f99c95f5bf28dc699fd1725
tcg-ethun <ethuntcg@gmail.com>
2025-07-04 10:49:58 +0600

    Updated File

--- 7.cpp
+++ 7.cpp
@@ -3,6 +3,20 @@ using namespace std;
 
 int main()
 {
-  
+    for(int i =0;i<=50;i++){
+          if(i%3==0 && i%5==0){
+            cout << "FizzBuzz" << endl;
+          }
+          else if (i%5==0){
+            cout << "Buzz" <<endl;
+          }
+          else if(i%3==0){
+            cout << "Fizz" <<endl;
+          }
+          else {
+            cout << "None" << endl;
+          }
+    }
+
 return 0;
 }
