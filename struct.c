c3ee632c71d750d61fb2af9093e2bb56ef762f6d
tcg-ethun <ethuntcg@gmail.com>
2025-07-16 19:52:18 +0600

    Updated File

--- struct.c
+++ struct.c
@@ -14,9 +14,25 @@ int main()
    op1.roll = 20;
    op1.cgp = 5.0;
    strcpy(op1.name,"Ethun");
-
    printf("Name is : %s \n",op1.name);
    printf("Roll is : %d \n",op1.roll);
    printf("Cgpa is : %.1f \n \n",op1.cgp);
+
+
+   struct student s2;
+   s2.roll = 30;
+   s2.cgp = 3.5;
+   strcpy(s2.name,"Tithi");
+  printf("Name is : %s \n",s2.name);
+  printf("Roll is : %d \n",s2.roll);
+  printf("Cgpa is : %.1f \n \n",s2.cgp);
+
+  struct student s3;
+  s3.roll = 40;
+  s3.cgp = 4.3;
+  strcpy(s3.name,"Shiuli");
+  printf("Name is : %s \n",s3.name);
+  printf("Roll is : %d \n",s3.roll);
+  printf("Cgpa is : %.1f \n",s3.cgp);
      return 0;
 }
