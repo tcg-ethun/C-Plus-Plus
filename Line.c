cfeac262c214c53488b3ad0bbc683f082132c82c
tcg-ethun <ethuntcg@gmail.com>
2025-07-17 08:39:57 +0600

    Updated File

--- Line.c
+++ Line.c
@@ -8,7 +8,8 @@ struct student {
 
 int main()
 {
-  struct student s1 ={20,3.5,"Esan"};
+  struct student s1 ={20,3.5,"Esan"};        ///Single line structure
+  s1.roll = 49;     //Change value 
   printf("Name is : %s \n" "Roll is : %d \n" "GPA is : %.1f",s1.name,s1.roll,s1.gpa);
 return 0;
 }
