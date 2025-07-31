54855118e25e63689026e14bc8a3d2b7426a9b08
tcg-ethun <ethuntcg@gmail.com>
2025-07-31 08:36:02 +0600

    Updated File

--- Practice/new.c
+++ Practice/new.c
@@ -1,6 +1,20 @@
 #include<stdio.h>
+#include<string.h>
+
+    struct student {
+       int  roll ;
+       float gpa;
+       char name[100];
+      };
+
 int main()
 {
- printf("Hello %s","Esan");
+   struct student ECO[100];
+   struct student RCO[100];
+   struct student FCO[100];
+
+
+   struct student s1={100,5.3,"Miss Etu"};
+   printf("Student roll : %d \nStudent GPA : %.1f  \nStudent name is : %s \n\n",s1.roll,s1.gpa,s1.name);
 return 0;
 }
